bf9ebdf0a9890982960cd15b85a3f4e5cbf713fb
JuanJose <juanjosemarcomendozam@gmail.com>
2023-09-09 03:30:39 -0500

    Cambios en diseño de registro de denuncia - Formato de inputs - Formmato de Textos - Botones activados / desactivados - Distribución de Card

--- src/app/shared/add-denuncia/add-denuncia.component.ts
+++ src/app/shared/add-denuncia/add-denuncia.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AddDenunciaComponent {
 
+  public cardSelected=1
+
+  nextCard(){
+    this.cardSelected=this.cardSelected+1
+  }
+  previousCard(){
+    this.cardSelected = this.cardSelected-1
+  }
 }
