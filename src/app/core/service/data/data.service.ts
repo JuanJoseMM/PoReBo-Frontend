440a0ca34c13038848761973bffb02a3297293e5
JuanJose <juanjosemarcomendozam@gmail.com>
2023-09-16 15:51:21 -0500

    llenando el frontedn

--- src/app/core/service/data/data.service.ts
+++ src/app/core/service/data/data.service.ts
@@ -1,11 +1,44 @@
 import { Injectable } from '@angular/core';
+import {  HttpClient,  HttpErrorResponse,  HttpHeaders} from '@angular/common/http'; 
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  public url="localhost:3000/api/v1"
-  constructor() { }
-
+  private url="http://localhost:3000/api/v1"
+  constructor(private http:HttpClient) { }
+  public async getPersonbyID(id:string):Promise<Observable<any>>{
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8',
+    });
+    return await this.http.get<any>(this.url+'/persons/dni/'+id,{
+      responseType:'json',
+      headers,
+    })
+  }
   
 }
+
+/**
+ *   public async getDataAnimacionRuta(cadena: any): Promise<void> {
+    console.log("Entrando a Animacion Ruta", cadena)
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8',
+      Authorization: 'Token ' + this.auth.getToken(),
+    });
+    let results: any;
+    const query = {
+      "STRID": cadena
+    };
+    results = await this.http.post(this.mainUrl + 'queryanimacion/', query, {
+      responseType: 'json',
+      headers,
+    })
+      .toPromise();
+    this.state.listaDatosAnimacionRuta = results[0]
+    console.log("Resultado Animacion", results, this.state.listaDatosAnimacionRuta)
+    return;
+  }
+
+ */
--- src/app/shared/add-denuncia/add-denuncia.component.ts
+++ src/app/shared/add-denuncia/add-denuncia.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { DataService } from 'src/app/core/service/data/data.service';
 
 @Component({
   selector: 'app-add-denuncia',
@@ -7,12 +9,63 @@ import { Component } from '@angular/core';
 })
 export class AddDenunciaComponent {
 
-  public cardSelected=5
-
+  public cardSelected=1
+  public isDisabled = true
+  private datosPersona:any
+  public personaForm = new FormGroup({
+    firstName : new FormControl(''),
+    lastName : new FormControl(''),
+    email : new FormControl(''),
+    dial_code : new FormControl(''),
+    phone : new FormControl(''),
+    gender : new FormControl(''),
+    maritalStatus : new FormControl(''),
+    nativeLanguage : new FormControl(''),
+    gradeInstruction : new FormControl(''),
+    status : new FormControl(''),
+    createAt : new FormControl(''),
+    updateAt : new FormControl(''),
+  })
+  constructor(private data:DataService){}
   nextCard(){
     this.cardSelected=this.cardSelected+1
   }
   previousCard(){
     this.cardSelected = this.cardSelected-1
+  }
+
+  async buscarByID(event: Event){
+    (await this.data.getPersonbyID('71993871')).subscribe(
+      respuesta => {
+        console.log(respuesta)
+        this.datosPersona = respuesta;
+        console.log(this.datosPersona)
+        this.cargarDatosPersona(this.datosPersona)
+      },
+      error => {
+        console.error('Hubo un error al obtener los datos', error);
+      }
+    );
+
+    
+
+  }
+
+  cargarDatosPersona(data:any){
+    console.log(data)
+    this.personaForm.patchValue({
+      firstName : data.firstName,
+      lastName : data.lastName,
+      email : data.email,
+      dial_code : data.dial_code,
+      phone : data.phone,
+      gender : data.gender,
+      maritalStatus : data.maritalStatus,
+      nativeLanguage : data.nativeLanguage,
+      gradeInstruction : data.gradeInstruction,
+      status : data.status,
+      createAt : data.createAt,
+      updateAt : data.updateAt,
+    })
   }
 }
