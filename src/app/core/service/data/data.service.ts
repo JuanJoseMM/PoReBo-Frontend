c564b6d26db03573ca762c7b591f45124bbd6907
Juan Jose <juanjosemarcomendozam@gmail.com>
2023-09-16 18:56:34 -0500

    cambios en denuncia

--- src/app/core/service/data/data.service.ts
+++ src/app/core/service/data/data.service.ts
@@ -1,24 +1,55 @@
 import { Injectable } from '@angular/core';
-import {  HttpClient,  HttpErrorResponse,  HttpHeaders} from '@angular/common/http'; 
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { format } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  private url="http://localhost:3000/api/v1"
-  constructor(private http:HttpClient) { }
-  public async getPersonbyID(id:string):Promise<Observable<any>>{
+  private url = "http://localhost:3000/api/v1"
+  constructor(private http: HttpClient) { }
+  public async getPersonbyID(id: string): Promise<Observable<any>> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json; charset=utf-8',
     });
-    return await this.http.get<any>(this.url+'/persons/dni/'+id,{
-      responseType:'json',
+    return await this.http.get<any>(this.url + '/persons/dni/' + id, {
+      responseType: 'json',
       headers,
     })
   }
-  
+  public async getLocationID(id: string): Promise<Observable<any>> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8',
+    });
+    return await this.http.get<any>(this.url + '/location/' + id, {
+      responseType: 'json',
+      headers,
+    })
+  }
+
+  public getTimeNow() {
+    const timeZone = 'America/Lima';
+
+    // Convierte la hora UTC a la hora de Perú
+    const peruTime = utcToZonedTime(new Date(), timeZone);
+
+    // Formatea la fecha y hora en el formato deseado
+    return format(peruTime, 'yyyy-MM-dd HH:mm');
+  }
+
+  public async getAgencyID(id: string): Promise<Observable<any>> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8',
+    });
+    return await this.http.get<any>(this.url + '/agency/' + id, {
+      responseType: 'json',
+      headers,
+    })
+  }
 }
+
 
 /**
  *   public async getDataAnimacionRuta(cadena: any): Promise<void> {
